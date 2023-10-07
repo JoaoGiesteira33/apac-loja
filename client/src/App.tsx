b1233854b738b9321ad816a33965d9503e83e276
João Luís Lopes Giesteira <joaollgiesteira@gmail.com>
2023-10-07 15:17:26 +0100

    MaterialUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '@mui/material/Button';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +22,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Button onClick={() => setCount((count) => count + 1)} variant="contained">count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
