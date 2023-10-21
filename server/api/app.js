3cf738c5ece37b989c1ac8b7e7d50dae41d8019f
Miyagi61 <pg50398@alunos.uminho.pt>
2023-10-22 00:00:42 +0100

    [UPDATE] ligação ao mongoDB Atlas com docker

--- server/api/app.js
+++ server/api/app.js
@@ -1,43 +1,38 @@
-const dotenv = require('dotenv');
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const mongoose = require('mongoose');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var mongoose = require('mongoose');
 
-dotenv.config();
+// // TODO - TROCAR OS NOMES DOS ROUTERS
+// var indexRouter = require('./routes/acordao');
+// var userRouter = require('./routes/user');
+// var sugestaoRouter = require('./routes/sugestao');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
-const app = express();
-app.use(cors());
-
-// DB connection
 var db_url = "mongodb+srv://"+process.env.MONGO_USER+":"+process.env.MONGO_PASSWORD+"@"+process.env.MONGO_CLUSTER+".mongodb.net/?retryWrites=true&w=majority";
 console.log(db_url)
 mongoose.connect(db_url, {dbName: process.env.MONGO_DB_NAME , useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error....'))
+db.on('open', function(){console.log("Conexão do Servidor de Autenticação ao MongoDB realizada com sucesso...")});
+db.on('error', function(){console.log("Erro de conexão ao MongoDB...")});
 
-db.once('open', () => {
-    console.log('Conexão ao MongoDB com sucesso!')
-})
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// TODO - ALTERAR PARA OS NOMES DEFINIDOS ACIMA
+//app.use('/acordaos', indexRouter);
+//app.use('/sugestoes', sugestaoRouter);
+//app.use('/user', userRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
