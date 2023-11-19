7290c7fc56335e05f4e0fd71018ef0bb74bf6c20
João Luís Lopes Giesteira <joaollgiesteira@gmail.com>
2023-11-19 14:52:55 +0000

    Fix carousel item width

--- client/src/components/ProductCarousel/ProductCarousel.tsx
+++ client/src/components/ProductCarousel/ProductCarousel.tsx
@@ -4,10 +4,7 @@ import ProductThumbnail from '../ProductThumbnail';
 import 'react-multi-carousel/lib/styles.css';
 import './styles.css';
 
-export default function ProductCarousel(props: {
-    title: string;
-    deviceType: string;
-}) {
+export default function ProductCarousel(props: { title: string }) {
     const items: ProductType[] = [
         {
             id: 1,
@@ -89,20 +86,104 @@ export default function ProductCarousel(props: {
             piece_info: null,
             book_info: null,
         },
+        {
+            id: 6,
+            title: 'Title6',
+            author: 'Author6',
+            thumbnailPhoto: 'https://picsum.photos/450/400',
+            photos: ['https://picsum.photos/450/400'],
+            price: {
+                amount: { EUR: 60 },
+                discount: 0,
+            },
+            description: 'Description6',
+            stock: 6,
+            product_type: 'product_type6',
+            piece_info: null,
+            book_info: null,
+        },
+        {
+            id: 7,
+            title: 'Title7',
+            author: 'Author7',
+            thumbnailPhoto: 'https://picsum.photos/450/400',
+            photos: ['https://picsum.photos/450/400'],
+            price: {
+                amount: { EUR: 70 },
+                discount: 0,
+            },
+            description: 'Description7',
+            stock: 7,
+            product_type: 'product_type7',
+            piece_info: null,
+            book_info: null,
+        },
+        {
+            id: 8,
+            title: 'Title8',
+            author: 'Author8',
+            thumbnailPhoto: 'https://picsum.photos/450/400',
+            photos: ['https://picsum.photos/450/400'],
+            price: {
+                amount: { EUR: 80 },
+                discount: 0,
+            },
+            description: 'Description8',
+            stock: 8,
+            product_type: 'product_type8',
+            piece_info: null,
+            book_info: null,
+        },
+        {
+            id: 9,
+            title: 'Title9',
+            author: 'Author9',
+            thumbnailPhoto: 'https://picsum.photos/450/400',
+            photos: ['https://picsum.photos/450/400'],
+            price: {
+                amount: { EUR: 90 },
+                discount: 0,
+            },
+            description: 'Description9',
+            stock: 9,
+            product_type: 'product_type9',
+            piece_info: null,
+            book_info: null,
+        },
     ];
     const responsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
+        desktop0: {
+            breakpoint: { max: 20000, min: 1800 },
+            items: 7,
+            partialVisibilityGutter: 40, // this is needed to tell the amount of px that should be visible.
+        },
+        desktop1: {
+            breakpoint: { max: 1800, min: 1500 },
+            items: 6,
+            partialVisibilityGutter: 40, // this is needed to tell the amount of px that should be visible.
+        },
+        desktop2: {
+            breakpoint: { max: 1500, min: 1200 },
+            items: 5,
+            partialVisibilityGutter: 40, // this is needed to tell the amount of px that should be visible.
+        },
+        desktop3: {
+            breakpoint: { max: 1200, min: 900 },
+            items: 4,
+            partialVisibilityGutter: 40, // this is needed to tell the amount of px that should be visible.
+        },
+        desktop4: {
+            breakpoint: { max: 900, min: 600 },
             items: 3,
             partialVisibilityGutter: 40, // this is needed to tell the amount of px that should be visible.
         },
         tablet: {
-            breakpoint: { max: 1024, min: 464 },
+            breakpoint: { max: 600, min: 300 },
             items: 2,
             partialVisibilityGutter: 30, // this is needed to tell the amount of px that should be visible.
         },
         mobile: {
-            breakpoint: { max: 464, min: 0 },
+            breakpoint: { max: 300, min: 0 },
             items: 1,
             partialVisibilityGutter: 30, // this is needed to tell the amount of px that should be visible.
         },
@@ -116,7 +197,6 @@ export default function ProductCarousel(props: {
                 draggable={false}
                 partialVisible={true}
                 keyBoardControl={true}
-                deviceType={props.deviceType}
                 containerClass="my-container-class"
                 itemClass="my-item-class">
                 {items.map((item) => (
