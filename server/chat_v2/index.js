8c8731479d6a079ddc08a9c3911eaaae8727dbe1
Miyagi61 <pg50398@alunos.uminho.pt>
2023-12-22 00:54:27 +0000

    utils criado no backend

--- server/chat_v2/index.js
+++ server/chat_v2/index.js
@@ -21,6 +21,8 @@ const sessionStore = new RedisSessionStore(redisClient);
 const { RedisMessageStore } = require("./messageStore");
 const messageStore = new RedisMessageStore(redisClient);
 
+const { send_email } = require("./utils");
+
 function isOnList(list, username) {
   for (let i = 0; i < list.length; i++) {
     if (list[i].username === username) {
@@ -171,38 +173,7 @@ io.on("connection", async (socket) => {
   console.log(">>>>>>>>>>>>>>>>>>>>")
 });
 
-const nodemailer = require('nodemailer');
 
-async function send_email(email, subject, text) {
-    try{
-        console.log("Email:",process.env.EMAIL);
-        console.log("Service:",process.env.SERVICE);
-        console.log("Port:",process.env.EMAIL_PORT);
-        console.log("Secure:",process.env.SECURE);
-        console.log("User:",process.env.USER_EMAIL);
-        console.log("Pass:",process.env.USER_PASS);
 
-        const transporter = nodemailer.createTransport({
-            host: process.env.EMAIL,
-            service: process.env.SERVICE,
-            port: process.env.EMAIL_PORT,
-            secure: process.env.SECURE,
-            auth: {
-                user: process.env.USER_EMAIL,
-                pass: process.env.USER_PASS
-            }
-        });
-        await transporter.sendMail({
-            from: process.env.USER_EMAIL,
-            to: email,
-            subject: subject,
-            text: text
-        });
-        
-        console.log("Email sent successfuly");
-    } catch(error){
-        console.log("Email not sent", error);
-    }
-}
 
 setupWorker(io);
