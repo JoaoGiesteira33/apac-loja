2aef3b1d6b9d5e3594c3513ab2363b6788ea88aa
João Luís Lopes Giesteira <joaollgiesteira@gmail.com>
2023-12-23 16:21:28 +0000

    Axios, Generic Hook for fetching, Grid for products

--- client/src/pages/pintar_o_7/Home.tsx
+++ client/src/pages/pintar_o_7/Home.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-
-import { SelectChangeEvent } from '@mui/material/Select';
-import Divider from '@mui/material/Divider';
+import React, { useState } from 'react';
 
 import Hero from '../../components/pintar_o_7/Hero';
 import MultipleSelectTypes from '../../components/pintar_o_7/MultipleSelectTypes';
+
+import Divider from '@mui/material/Divider';
 import Box from '@mui/system/Box';
 import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Unstable_Grid2';
+
+import useProductSearch from '../../hooks/useProductSearch';
 
 export default function Home() {
+    const [productQuery, setProductQuery] = useState('');
+    const [productPage, setProductPage] = useState(1);
     const [selectedTypes, setSelectedTypes] = React.useState<string[]>([]);
+
+    const { products, hasMore, loading, error } = useProductSearch(
+        productQuery,
+        productPage
+    );
 
     return (
         <Box component="div">
@@ -31,6 +40,24 @@ export default function Home() {
                 </Box>
             </Stack>
             <Divider variant="middle" />
+            <Grid container spacing={4}>
+                {products &&
+                    products.map((product, index) => (
+                        <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
+                            <>{product}</>
+                        </Grid>
+                    ))}
+                {error && <div>Error</div>}
+                {loading && <div>Loading...</div>}
+                {hasMore && (
+                    <button
+                        onClick={() => {
+                            setProductPage((prevPageNumber) => prevPageNumber + 1);
+                        }}>
+                        Load More
+                    </button>
+                )}
+            </Grid>
             <Divider variant="middle" />
         </Box>
     );
