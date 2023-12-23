1c8da57e5ac752a641fc7167a5bf7a7d266be867
João Luís Lopes Giesteira <joaollgiesteira@gmail.com>
2023-12-23 18:40:47 +0000

    Inicio product thumbnail

--- client/src/pages/pintar_o_7/Home.tsx
+++ client/src/pages/pintar_o_7/Home.tsx
@@ -4,18 +4,22 @@ import Hero from '../../components/pintar_o_7/Hero';
 import MultipleSelectTypes from '../../components/pintar_o_7/MultipleSelectTypes';
 
 import Divider from '@mui/material/Divider';
+import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/system/Box';
+import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Unstable_Grid2';
+import AddCircleOutlineSharpIcon from '@mui/icons-material/AddCircleOutlineSharp';
 
 import useProductSearch from '../../hooks/useProductSearch';
+import ProductThumbnail from '../../components/pintar_o_7/ProductThumbnail';
 
 export default function Home() {
     const [productQuery, setProductQuery] = useState('');
     const [productPage, setProductPage] = useState(1);
     const [selectedTypes, setSelectedTypes] = React.useState<string[]>([]);
 
-    const { products, hasMore, loading, error } = useProductSearch(
+    const { MockData, hasMore, loading, error } = useProductSearch(
         productQuery,
         productPage
     );
@@ -40,24 +44,56 @@ export default function Home() {
                 </Box>
             </Stack>
             <Divider variant="middle" />
-            <Grid container spacing={4}>
-                {products &&
-                    products.map((product, index) => (
-                        <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
-                            <>{product}</>
-                        </Grid>
-                    ))}
-                {error && <div>Error</div>}
-                {loading && <div>Loading...</div>}
-                {hasMore && (
-                    <button
-                        onClick={() => {
-                            setProductPage((prevPageNumber) => prevPageNumber + 1);
-                        }}>
-                        Load More
-                    </button>
-                )}
-            </Grid>
+
+            <Box
+                component="div"
+                sx={{
+                    paddingX: {
+                        xs: '2rem',
+                        sm: '4rem',
+                        md: '6rem',
+                        lg: '8rem',
+                    },
+                    paddingTop: '3rem',
+                }}>
+                <Grid
+                    container
+                    sx={{
+                        justifyContent: { xs: 'center', sm: 'space-between' },
+                    }}
+                    spacing={{ xs: 2, md: 4, lg: 8 }}>
+                    {MockData &&
+                        MockData.map((product, index) => (
+                            <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
+                                <ProductThumbnail product={product} />
+                            </Grid>
+                        ))}
+                </Grid>
+            </Box>
+
+            {error && <div>Error</div>}
+            {loading && (
+                <Box
+                    component="div"
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        marginY: '2rem',
+                    }}>
+                    <CircularProgress />
+                </Box>
+            )}
+            {hasMore && (
+                <Button
+                    startIcon={<AddCircleOutlineSharpIcon />}
+                    variant="outlined"
+                    onClick={() => {
+                        setProductPage((prevPageNumber) => prevPageNumber + 1);
+                    }}>
+                    Load More
+                </Button>
+            )}
             <Divider variant="middle" />
         </Box>
     );
