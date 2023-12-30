f071bd2c2fefa55b3fbfd2301bce029cc73e504c
pedrommf7 <pedro2mferreira@gmail.com>
2023-12-30 18:46:17 +0000

    copy utils folder to the containers

--- server/run.sh
+++ server/run.sh
@@ -15,6 +15,7 @@ echo "  [Building image 'apac_api_img' and put files inside]"
 docker build ./api -t apac_api_img
 docker create --name temp_container_api apac_api_img
 docker cp api temp_container_api:/code/
+docker cp utils temp_container_api:/code/
 docker commit temp_container_api apac_api_img
 docker rm temp_container_api -f
 
@@ -23,6 +24,7 @@ echo "  [Building image 'apac_auth_img' and put files inside]"
 docker build ./auth -t apac_auth_img
 docker create --name temp_container_auth apac_auth_img
 docker cp auth temp_container_auth:/code/
+docker cp utils temp_container_auth:/code/
 docker commit temp_container_auth apac_auth_img
 docker rm temp_container_auth -f
 
