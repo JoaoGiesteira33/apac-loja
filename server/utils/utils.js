92b557e64b243cb56af22f6a8569bbafe895d600
pedrommf7 <pedro2mferreira@gmail.com>
2024-01-03 18:12:38 +0000

    verificacoes movidas para o utils

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -32,6 +32,94 @@ async function send_email(email, subject, text) {
     }
 }
 
+function getDateTime() {
+	const date = new Date();
+	const utcTime = date.getTime() + (date.getTimezoneOffset() * 60000);
+
+	// var timeOffset = 0;
+	// var PortugalTime = new Date(utcTime + (3600000 * timeOffset));
+
+	// time offset for Portugal is 0, so:
+	var PortugalTime = new Date(utcTime)
+
+	return PortugalTime.toISOString().substring(0, 19)
+}
+
+function isAdmin(req, res, next) {
+	var myToken = req.query.token || req.body.token || req.cookies.token
+	if (myToken) {
+		jwt.verify(myToken, secrets.AUTH_KEY, function (e, payload) {
+			if (e) {
+				res.status(403).jsonp({ error: "Invalid Token!" })
+			}
+			else if (payload.level == "admin") {
+				req.user = payload.username
+				req.level = payload.level
+				req.token = myToken
+				next()
+			}
+			else {
+				res.status(401).render({ error: "Access denied!" })
+			}
+		})
+	}
+	else {
+		res.status(400).jsonp({ error: "Token not found!" })
+	}
+}
+
+function hasAccess(req, res, next) {
+	var myToken = req.query.token || req.body.token || req.cookies.token
+	if (myToken) {
+		jwt.verify(myToken, secrets.AUTH_KEY, function (e, payload) {
+			if (e) {
+				res.status(403).jsonp({ error: "Invalid Token!" })
+			}
+			else {
+				req.user = payload.username
+				req.level = payload.level
+				req.token = myToken
+				next()
+			}
+		})
+	}
+	else {
+		res.status(400).jsonp({ error: "Token not found!" })
+	}
+}
+
+function isMe(req, res, next) {
+	var myToken = req.query.token || req.body.token || req.cookies.token
+	if (myToken) {
+		if (req.params.username) {
+			jwt.verify(myToken, secrets.AUTH_KEY, function (e, payload) {
+				if (e) {
+					res.status(403).jsonp({ error: "Invalid Token!" })
+				}
+				else if (payload.username == req.params.username) {
+					req.user = payload.username
+					req.level = payload.level
+					req.token = myToken
+					next()
+				}
+				else {
+					res.status(401).jsonp({ error: "Access denied!" })
+				}
+			})
+		}
+		else {
+			res.status(400).jsonp({ error: "Falta de parametros" })
+		}
+	}
+	else {
+		res.status(400).jsonp({ error: "Token not found!" })
+	}
+}
+
 module.exports = {
-    send_email
+    send_email,
+    getDateTime,
+    isAdmin,
+    hasAccess,
+    isMe
 }
