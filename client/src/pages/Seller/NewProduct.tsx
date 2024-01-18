d713ece4eab8e81d7943d0ea71ccfe6cb697bc4d
João Luís Lopes Giesteira <joaollgiesteira@gmail.com>
2024-01-18 00:11:32 +0000

    Continuacao Produto

--- client/src/pages/Seller/NewProduct.tsx
+++ client/src/pages/Seller/NewProduct.tsx
@@ -1,16 +1,34 @@
 import {
+    Autocomplete,
     Box,
+    Chip,
     FormControl,
     Input,
     InputAdornment,
     InputLabel,
+    MenuItem,
     Paper,
+    Select,
     Stack,
     TextField,
     Typography,
 } from '@mui/material';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import SelectTypes from '../../components/pintar_o_7/SelectType';
+
+const availableTypes: string[] = [
+    'Pintura',
+    'Escultura',
+    'Fotografia',
+    'Desenho',
+    'Colagens',
+    'Impressões e Gravuras',
+    'Arte Digital',
+    'Instalação',
+    'Desing',
+    'Arte Têxtil',
+];
 
 export default function NewProduct() {
     const { t } = useTranslation();
@@ -18,6 +36,13 @@ export default function NewProduct() {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState(9.99);
+    const [selectedTypes, setSelectedTypes] = useState<string>('');
+    const [technique, setTechnique] = useState('');
+    const [materials, setMaterials] = useState<string>('');
+    const [width, setWidth] = useState<string>('');
+    const [height, setHeight] = useState<string>('');
+    const [depth, setDepth] = useState<string>('');
+    const [measureUnit, setMeasureUnit] = useState<string>('');
 
     return (
         <Box
@@ -45,7 +70,7 @@ export default function NewProduct() {
                     }}>
                     <Typography variant="h4">
                         {t('artist.new-piece-page.paper-basic-info')}
-                    </Typography>{' '}
+                    </Typography>
                     <TextField
                         variant="standard"
                         margin="normal"
@@ -71,11 +96,15 @@ export default function NewProduct() {
                         value={description}
                         onChange={(e) => setDescription(e.target.value)}
                     />
-                    <Stack>
-                        <FormControl
-                            sx={{ margin: '16px 8px 0px 0px' }}
-                            fullWidth
-                            variant="standard">
+                    <Stack
+                        direction={{
+                            xs: 'column',
+                            sm: 'row',
+                        }}
+                        spacing={{ xs: 2, sm: 4 }}
+                        sx={{ marginTop: '1rem' }}
+                        alignItems={'center'}>
+                        <FormControl fullWidth variant="standard">
                             <InputLabel htmlFor="standard-adornment-amount">
                                 {t('global.price')}
                             </InputLabel>
@@ -88,6 +117,107 @@ export default function NewProduct() {
                                 }
                             />
                         </FormControl>
+                        <FormControl
+                            variant="standard"
+                            sx={{
+                                m: 1,
+                                minWidth: 120,
+                                margin: '0',
+                            }}
+                            fullWidth>
+                            <InputLabel id="select-type-label">
+                                {t('global.type-of-piece')}
+                            </InputLabel>
+                            <Select
+                                labelId="select-type-label"
+                                id="demo-simple-select-standard"
+                                value={selectedTypes}
+                                onChange={(e) =>
+                                    setSelectedTypes(e.target.value)
+                                }
+                                label={t('global.types')}>
+                                {availableTypes.map((tp, index) => (
+                                    <MenuItem key={index} value={tp}>
+                                        {tp}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Stack>
+                </Paper>
+                <Paper
+                    sx={{
+                        width: '100%',
+                        padding: '2rem',
+                    }}>
+                    <Typography variant="h4">
+                        {t('artist.new-piece-page.paper-details')}
+                    </Typography>
+                    <TextField
+                        variant="standard"
+                        margin="normal"
+                        label={t('product.technique')}
+                        type="text"
+                        fullWidth
+                        error={false}
+                        helperText={''}
+                        id="technique"
+                        value={technique}
+                        onChange={(e) => setTechnique(e.target.value)}
+                    />
+                    <Autocomplete
+                        multiple
+                        id="materials"
+                        options={[]}
+                        freeSolo
+                        renderTags={(value: readonly string[], getTagProps) =>
+                            value.map((option: string, index: number) => (
+                                <Chip
+                                    variant="outlined"
+                                    label={option}
+                                    {...getTagProps({ index })}
+                                />
+                            ))
+                        }
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                variant="standard"
+                                label={t('product.materials')}
+                                placeholder={t('product.materials-placeholder')}
+                            />
+                        )}
+                    />
+                    <Stack
+                        direction={'row'}
+                        spacing={2}
+                        sx={{ marginTop: '1rem' }}
+                        alignItems={'center'}>
+                        <TextField
+                            variant="standard"
+                            margin="normal"
+                            label={t('product.width')}
+                            type="text"
+                            fullWidth
+                            error={false}
+                            helperText={''}
+                            id="width"
+                            value={width}
+                            onChange={(e) => setWidth(e.target.value)}
+                        />
+                        <Typography alignSelf={'flex-end'}>X</Typography>
+                        <TextField
+                            variant="standard"
+                            margin="normal"
+                            label={t('product.height')}
+                            type="text"
+                            fullWidth
+                            error={false}
+                            helperText={''}
+                            id="height"
+                            value={height}
+                            onChange={(e) => setHeight(e.target.value)}
+                        />
                     </Stack>
                 </Paper>
             </Stack>
